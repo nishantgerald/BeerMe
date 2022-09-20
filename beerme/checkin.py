dacf5c424aa026ad2f585c5ee52dfa0ac36c6f52
Nishant Gerald <nishant.gerald@gmail.com>
2022-09-19 21:28:22 -0400

    initialized repo

---
+++ beerme/checkin.py
@@ -0,0 +1,50 @@
+import functools
+from datetime import datetime
+from flask import (
+    Blueprint,
+    flash,
+    g,
+    redirect,
+    render_template,
+    request,
+    session,
+    url_for,
+)
+from werkzeug.security import check_password_hash, generate_password_hash
+from beerme.db import get_db
+
+# DEFINING THE BLUEPRINT CALLED `AUTH`
+bp = Blueprint("checkin", __name__, url_prefix="/checkin")
+
+@bp.route("/log_beer", methods=("GET", "POST"))
+def log_beer():
+    if request.method == "POST":
+        CHECKIN_DATE = datetime.today().strftime("%Y-%m-%d %H:%M:%S")
+        BEER_NAME = request.form["BEER_NAME"].lower()
+        BEER_TYPE = request.form["BEER_TYPE"].lower()
+        BREWERY_NAME = request.form["BREWERY_NAME"].lower()
+        BEER_RATING = request.form["BEER_RATING"]
+        db = get_db()
+        error = None
+
+        if not BEER_NAME:
+            error = "Beer name is required."
+        elif not BREWERY_NAME:
+            error = "Brewery name is required."
+        elif not BEER_RATING:
+            error = "Rating is required."
+
+        if error is None:
+            try:
+                db.execute(
+                    "INSERT INTO beers (CHECKIN_DATE, BEER_NAME, BEER_TYPE, BREWERY_NAME, BEER_RATING) VALUES (?, ?, ?, ?, ?)",
+                    (CHECKIN_DATE, BEER_NAME, BEER_TYPE, BREWERY_NAME, BEER_RATING),
+                )
+                db.commit()
+            except db.IntegrityError:
+                error = f"{BEER_NAME} has already been checked in before."
+            # else:
+            #     return redirect(url_for("auth.login"))
+        if error is not None:
+            flash(error)
+    return render_template("checkin/log_beer.html")
