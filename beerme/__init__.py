647707074e15b64a031fdcb02a4b38ba07cb6b37
Nishant Gerald <nishant.gerald@gmail.com>
2022-09-21 15:05:20 -0400

    registered auth blueprint with app

--- beerme/__init__.py
+++ beerme/__init__.py
@@ -25,6 +25,9 @@ def create_app(test_config=None):
     from . import db
     db.init_app(app)
 
+    from . import auth
+    app.register_blueprint(auth.bp)
+
     from . import checkin
     app.register_blueprint(checkin.bp)
     
