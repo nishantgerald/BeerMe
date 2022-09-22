9cc3b408e991d3c2b5c8a7916920cb81bc4cb415
Nishant Gerald <nishant.gerald@gmail.com>
2022-09-21 22:13:52 -0400

    included two functions to generate beer type and rating histograms

--- beerme/stats.py
+++ beerme/stats.py
@@ -14,7 +14,11 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from beerme.db import get_db
 from beerme.auth import login_required
 import pandas as pd
-import hashlib
+import matplotlib.pyplot as plt
+plt.switch_backend('Agg')
+import io
+import base64
+import os
 
 # DEFINING THE BLUEPRINT CALLED `stats`
 bp = Blueprint("stats", __name__, url_prefix="/stats")
@@ -29,14 +33,45 @@ def get_stats():
         select
             beer_name as beer,
             brewery_name as brewery,
-            date(CHECKIN_DATE) as date
+            beer_type as type,
+            date(CHECKIN_DATE) as date,
+            beer_rating as rating
         from
             beers
         where
             username = '{g.user['username']}'
         order by datetime(CHECKIN_DATE) desc
-        limit
-            5
         '''
         , db)
-    return render_template("stats/get_stats.html", last_five_beers_df=last_five_beers_df)
+    
+    BEER_TYPES_HIST_IMAGE_NAME=f"{g.user['username']}_beer_types_plot.png"
+    generate_beer_type_histogram(last_five_beers_df,BEER_TYPES_HIST_IMAGE_NAME)
+
+    BEER_RATINGS_HIST_IMAGE_NAME=f"{g.user['username']}_beer_ratings_plot.png"
+    generate_beer_ratings_histogram(last_five_beers_df,BEER_RATINGS_HIST_IMAGE_NAME)
+
+    return render_template("stats/get_stats.html", last_five_beers_df=last_five_beers_df.head(5), beer_types_histogram=BEER_TYPES_HIST_IMAGE_NAME, beer_ratings_histogram=BEER_RATINGS_HIST_IMAGE_NAME)
+
+def generate_beer_type_histogram(dataframe,IMAGE_NAME):
+    # GENERATE BEER TYPE HISTOGRAM
+    try:
+        os.remove(IMAGE_NAME)
+    except:
+        pass
+    root_dir = os.path.dirname(os.getcwd())
+    IMG_PATH=os.path.join(root_dir, 'BeerMe', 'beerme', 'static', f'{IMAGE_NAME}')
+    plt.clf()
+    plt.hist(dataframe.type, color='#eedb02', bins=20)
+    plt.savefig(IMG_PATH)
+
+def generate_beer_ratings_histogram(dataframe,IMAGE_NAME):
+    # GENERATE BEER TYPE HISTOGRAM
+    try:
+        os.remove(IMAGE_NAME)
+    except:
+        pass
+    root_dir = os.path.dirname(os.getcwd())
+    IMG_PATH=os.path.join(root_dir, 'BeerMe', 'beerme', 'static', f'{IMAGE_NAME}')
+    plt.clf()
+    plt.hist(dataframe.rating, color='#54b8f9', bins=20)
+    plt.savefig(IMG_PATH)
